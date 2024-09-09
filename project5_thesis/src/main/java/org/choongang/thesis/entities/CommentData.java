76a8cc9458a6e165550c82b1d1152db03534e0e9
yonggyo1125 <yonggyo00@kakao.com>
2024-09-09 15:29:47 +0900

    .

---
+++ project5_thesis/src/main/java/org/choongang/thesis/entities/CommentData.java
@@ -0,0 +1,31 @@
+package org.choongang.thesis.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.choongang.global.entities.BaseEntity;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentData extends BaseEntity {
+    @Id @GeneratedValue
+    private Long col;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Thesis thesis;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @Column(length=20)
+    private String ip;
+
+    @Column(length=150)
+    private String ua;
+}
