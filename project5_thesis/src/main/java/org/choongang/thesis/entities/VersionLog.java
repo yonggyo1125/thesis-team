f97a7dec39400bd5d65fca31d963b60d6f823d3b
yonggyo1125 <yonggyo00@kakao.com>
2024-09-09 15:19:34 +0900

    .

--- project5_thesis/src/main/java/org/choongang/thesis/entities/VersionLog.java
+++ project5_thesis/src/main/java/org/choongang/thesis/entities/VersionLog.java
@@ -1,6 +1,6 @@
 package org.choongang.thesis.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,4 +13,18 @@ import org.choongang.global.entities.BaseMemberEntity;
 @NoArgsConstructor
 @AllArgsConstructor
 public class VersionLog extends BaseMemberEntity {
+    @Id @GeneratedValue
+    private Long logSeq;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="tid")
+    private Thesis thesis;
+    private int major; // 주버전
+    private int minor; // 부버전
+
+    @Lob
+    private String before; // 변경전 데이터
+
+    @Lob
+    private String after; // 변경 후 데이터
 }
