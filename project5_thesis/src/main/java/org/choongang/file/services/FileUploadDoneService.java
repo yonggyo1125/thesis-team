d077fd734b20f35c904c5acb711db0be3aedfd4d
yonggyo1125 <yonggyo00@kakao.com>
2024-09-10 11:35:10 +0900

    .

--- project5_thesis/src/main/java/org/choongang/file/services/FileUploadDoneService.java
+++ project5_thesis/src/main/java/org/choongang/file/services/FileUploadDoneService.java
@@ -1,9 +1,21 @@
 package org.choongang.file.services;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.global.Utils;
+import org.choongang.global.exceptions.BadRequestException;
+import org.choongang.global.rests.ApiRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class FileUploadDoneService {
+    private final ApiRequest apiRequest;
+    private final Utils utils;
+
+    public void process(String gid) {
+        ApiRequest result = apiRequest.request("/done/" + gid, "file-service");
+        if (!result.getStatus().is2xxSuccessful()) {
+            throw new BadRequestException(utils.getMessage("Fail.file.done"));
+        }
+    }
 }
